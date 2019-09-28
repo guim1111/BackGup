a4ae359fad562663b2dee449b341146e990d9b18
guim1111 <guim111@hotmail.com>
2019-09-28 17:35:50 +0200

    config threads on

--- BackGup/backGup.py
+++ BackGup/backGup.py
@@ -4,6 +4,12 @@ Created on 19 jun. 2019
 @author: Guim
 '''
 import configparser
+
 config = configparser.ConfigParser()
 config.read("config.ini")
 config.sections()
+threads = config['BASIC']['ThreadsON']
+listThreads = threads.split("::")
+
+for thread in listThreads:
+    print(config[thread]["Home"])
